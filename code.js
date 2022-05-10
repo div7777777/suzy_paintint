b8aa29e0fef3cae18b8641a26056c0ef817c4db9
div7777777 <99870561+div7777777@users.noreply.github.com>
2022-05-10 01:00:03 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,18 @@
+// menu responsive
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+//   burger
+  // réseaux sociaux
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
